9abe058f586b19c0f416ed521f12b7fbae85425a
hjoerdiskrohn <hmkr58281@edu.ucl.dk>
2024-03-20 10:09:09 +0100

    Indsættelse af billedegalleri

---
+++ js/billedegaleri.js
@@ -0,0 +1,31 @@
+let slideIndex2 = 1;
+showSlides(slideIndex2);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex2 = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("billedegalleri");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("selveSkriften");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+  captionText.innerHTML = dots[slideIndex2-1].alt;
+}
+
