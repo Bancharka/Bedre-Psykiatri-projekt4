48e3cc44e15c442631236f35921ac177eff9c020
Bancharka <olipet101@gmail.com>
2024-04-11 12:51:17 +0200

    Burgermenu
    Den har fået et 'if' statement så den passer i de krav vi kan huske skal være med

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,27 +1,32 @@
+
 let menuOptions = [
-    { title: "Få hjælp", url: "https://bedrepsykiatri.dk/hjaelp/"},
-    { title: "Videncenter", href: "https://bedrepsykiatri.dk/viden/"},
-    { title: "Om os", url: "https://bedrepsykiatri.dk/om/"},
-    { title: "Bliv friviilig", url: "#" },
-    { title: "Presse og politik", url:"https://bedrepsykiatri.dk/pressepolitik/"},
-    { title: "Støt os", url:"https://bedrepsykiatri.dk/stoet/"},
-  ];
+  { title: "Få hjælp", url: "https://bedrepsykiatri.dk/hjaelp/"},
+  { title: "Videncenter", href: "https://bedrepsykiatri.dk/viden/"},
+  { title: "Om os", url: "https://bedrepsykiatri.dk/om/"},
+  { title: "Bliv frivillig", url: "#" },
+  { title: "Presse og politik", url:"https://bedrepsykiatri.dk/pressepolitik/"},
+  { title: "Støt os", url:"https://bedrepsykiatri.dk/stoet/"},
+];
+
+function openNav() {
+  let sidenav = document.getElementById("mySidenav");
+  sidenav.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
+  // løb igennem array og tilføj menuoptioner
+  menuOptions.forEach(function(option) {
+      let link = document.createElement("a");
+      link.href = option.url;
+      link.textContent = option.title;
+      sidenav.appendChild(link);
+  });
+ // åben menu
+  sidenav.style.width = "210px";
   
-  function openNav() {
-    let sidenav = document.getElementById("mySidenav");
-    sidenav.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
-    // løb igennem array og tilføj menuoptioner
-    menuOptions.forEach(function(option) {
-        let link = document.createElement("a");
-        link.href = option.url;
-        link.textContent = option.title;
-        sidenav.appendChild(link);
-    });
-   //åben menu
-    sidenav.style.width = "210px";
+
+  if (2 + 2 === 4) {
+      console.log("Hello World");
   }
-  
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
